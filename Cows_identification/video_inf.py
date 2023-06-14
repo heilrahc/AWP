e3f9da82a9b01abc59a8061c489b71361f30537f
Rokessovsky <charlie.h512420@gmail.com>
2023-06-13 23:00:23 -0700

    video2

--- Cows_identification/video_inf.py
+++ Cows_identification/video_inf.py
@@ -5,6 +5,6 @@ import os
 data_path = "/home/mine01/Desktop/code/AWP/Cows_identification/cows_detect_data.yaml"
 model = YOLO('yolov8m.pt')
 
-model.train(data=data_path, epochs=50, imgsz=640)
+# model.train(data=data_path, epochs=50, imgsz=640)
 
 results = model('/home/mine01/Desktop/code/AWP/Cows_identification/videos/4008/010421_MVI_0114_short_22.MP4', save=True)
